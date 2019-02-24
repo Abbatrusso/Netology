444e3aca53d2407941c8c721eebf3b5509c898de
Abbatrusso <47182518+Abbatrusso@users.noreply.github.com>
2019-02-24 21:35:48 +0300

    Update total1.sql

--- total1.sql
+++ total1.sql
@@ -46,4 +46,68 @@ INSERT INTO Employee VALUES
 ('19', '6', '9', 'Jessy', 19),
 ('20', '6', '9', 'Ann', 18);
 
+-- A. Вывести список названий департаментов и количество главных врачей в каждом из этих департаментов
+
+SELECT 
+    Department.name, 
+    COUNT(DISTINCT(Employee.chief_doc_id))
+FROM Department
+RIGHT JOIN Employee ON Department.id = Employee.department_id
+GROUP BY Department.name;
+-- B. Вывести список департаментов, в которых работают 3 и более сотрудников (id и название департамента, количество сотрудников)
+
+SELECT 
+    Department.name,
+    Department.id,
+    COUNT(DISTINCT(Employee.name))
+FROM Department
+RIGHT JOIN Employee ON Department.id = Employee.department_id
+GROUP BY Department.name, Department.id
+HAVING COUNT(DISTINCT(Employee.name)) >= 3
+
+
+-- C. Вывести список департаментов с максимальным количеством публикаций  (id и название департамента, количество публикаций)
+
+SELECT
+    Department.id, 
+    Department.name,
+    MAX(Employee.num_public) AS max_pub
+FROM Department
+RIGHT JOIN Employee ON Department.id = Employee.department_id
+GROUP BY Department.name, Department.id
+ORDER BY Department.id;
+
+-- D. Вывести список сотрудников с минимальным количеством публикаций в своем департаменте (id и название департамента, имя сотрудника, количество публикаций)
+
+-- Берет только одного автора статей с мин.кол-ом, не работает когда авторов > 1 
+
+WITH temp_t AS 
+(
+  SELECT
+    DISTINCT(department_id) AS id_d,
+    Department.name AS name_d,
+    Employee.name AS name_e,
+    MIN(num_public) OVER (PARTITION BY department_id) AS min_pub,
+    num_public AS num_pub
+  FROM Employee
+  JOIN Department ON department_id = Department.id 
+  ORDER BY id_d
+)
+
+SELECT id_d, name_d, name_e, min_pub
+FROM temp_t
+WHERE num_pub = min_pub
+
+-- E. Вывести список департаментов и среднее количество публикаций для тех департаментов, в которых работает более одного главного врача (id и название департамента, среднее количество публикаций)
+SELECT
+    Department.id, 
+    Department.name,
+    AVG(Employee.num_public) AS avg_pub
+FROM Department
+RIGHT JOIN Employee ON Department.id = Employee.department_id
+GROUP BY Department.name, Department.id
+HAVING COUNT(DISTINCT(Employee.chief_doc_id)) > 1
+
+
+
 
